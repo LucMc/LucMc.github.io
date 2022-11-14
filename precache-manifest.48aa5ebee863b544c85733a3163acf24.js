239c5649947660453a5150b0b39eb9ada6504f91
Luc McCutcheon <lucmccutcheon.home@surrey.ac.uk>
2022-11-14 11:11:58 +0000

    Updates

---
+++ precache-manifest.48aa5ebee863b544c85733a3163acf24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "527b9f8ae9794b7f7b94232ae491a2b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c01609fef2b4f44da427",
+    "url": "/static/css/main.23a42523.chunk.css"
+  },
+  {
+    "revision": "7355874f27c05a80a7db",
+    "url": "/static/js/2.7ee65955.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.7ee65955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c01609fef2b4f44da427",
+    "url": "/static/js/main.ae18d96c.chunk.js"
+  },
+  {
+    "revision": "97c002b8f8a25334b102",
+    "url": "/static/js/runtime-main.67d84520.js"
+  }
+]);
